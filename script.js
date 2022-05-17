9dc1bb646593f1bb2f93a917bfa7b35ace889094
Marcella Mendoza <99848492+MarcellaMendoza@users.noreply.github.com>
2022-05-17 05:30:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,208 @@
+// const welcomeElement = document.getElementById("welcome-element")
+
+// function greetUser(greeting, name, emoji) {
+//     welcomeElement.textContent = `${greeting}, ${name} ${emoji}`
+// }
+
+// greetUser("Hello", "James Joyce", "🖋")
+
+// function addThem(number1, number2) {
+//   return number1 + number2
+// }
+
+// console.log(addThem(32, 2))
+
+// function getFirst(array) {
+//   return array[0]
+// }
+
+// let firstThing = getFirst([55, 235, 132344])
+
+// console.log(firstThing)
+
+//Practice 1 2 3 4
+// const welcomeElement = document.getElementById("welcome-element")
+
+// function greetUser (greeting, name, emoji) {
+//   welcomeElement.textContent = `${greeting} ${name} ${emoji}`
+  
+// }
+
+// greetUser ("Welcome back", "Marcella", "🖋")
+
+// //Practice 5
+
+// function multiplyNumbers(number1, number2) {
+//   return 3 * 9
+// }
+
+// console.log(multiplyNumbers())
+
+// //Practice 6
+// let secondItem = getSecond(["Marcella", "Sebastian", "Oscar"])
+
+// function getSecond(myArray) {
+//   return myArray[1]
+// }
+
+// console.log(secondItem)
+// //
+
+// let thirdItem = getThird(["watermelon", "cucumbers", "carrots"])
+
+// function getThird(myArray) {
+//   return myArray[2]
+// // }
+
+// // console.log(thirdItem)
+
+// //Practice 7: Lindros' Lascivious Limburger
+
+// const expectedMinutesInOven = 40
+// const preparationMinutesPerLayer = 2
+
+
+// function remainingMinutesInOven(actualMinutesInOven) {
+//   return expectedMinutesInOven - actualMinutesInOven
+// }
+
+
+// function preparationTimeInMinutes(numberOfLayers) {
+//    return numberOfLayers * preparationMinutesPerLayer
+//  }
+
+// function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
+//   return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven
+// }
+
+// console.log(totalTimeInMinutes(5, 5))
+
+// //Practice 1
+//  const welcomeElement = document.getElementById("welcome-element")
+
+// function greetUser(greeting, ) {
+//   welcomeElement.textContent = greeting
+// }
+
+// greetUser("Hello Marcella!")
+// //////////////////////////////
+// const welcomeElement = document.getElementById("welcome-element")
+
+// function greetUser(greeting, name, emoji) {
+//     welcomeElement.textContent = `${greeting}, ${name} ${emoji}`
+// }
+
+// greetUser("Hello", "James Joyce", "🖋")
+
+
+// const welcomeElement = document.getElementById("welcome-element")
+
+// function greetUser(greeting, name, emoji) {
+//   welcomeElement.textContent = ``
+// }
+
+
+
+
+// //Practice 1
+// const welcomeElement = document.getElementById("welcome-element")
+
+// function greetUser(greeting , name, goodbye) {
+//   welcomeElement.textContent = `${greeting} ${name} ${goodbye}`
+// }
+
+// greetUser("Hello", "Marcella", "have a good evening!" )
+
+
+
+// function multiply(number1, number2) {
+//   console.log(number1 * number2)
+ 
+// }
+
+// multiply(10, 5)
+
+
+// // function add(number1, number2, number3) {
+//   console.log(number1 + number2 + number3)
+// }
+
+// add(5, 0, 4)
+
+// // function divide(number1, number2) {
+// //   console.log(number1/number2)
+// // }
+
+// // divide(12, 4)
+
+// function subtract(number1, number2) {
+//   console.log(number2 - number1)
+// }
+
+// subtract(10, 100)
+// /////
+// //Practice 5
+// let secondItem = getSecond(["marcella", "Sebas", "oscar"])
+
+// function getSecond(myArray) {
+//   return (myArray[1])
+
+// }
+
+// console.log(secondItem)
+
+// // 
+
+// let thirdItem = getThird(["marcella", "Sebas", "oscar"])
+
+// function getThird(myArray) {
+//   return myArray[2]
+// }
+
+// console.log(thirdItem)
+// //
+// let firstItem = getFirst(["marcella", "Sebas", "oscar"])
+
+// function getFirst(myArray) {
+//   return myArray[0]
+// }
+
+// console.log(firstItem)
+// ////////////////
+
+const expectedMinutesInOven = 40
+
+function remainingMinutesInOven(actualMinutesInOven) {
+  return expectedMinutesInOven - actualMinutesInOven
+}
+
+
+function preparationTimeInMinutes(numberOfLayers) {
+  return numberOfLayers * 2
+}
+
+function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
+  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven
+}
+
+console.log(totalTimeInMinutes(5, 5))
+
+
+// const expectedMinutesInOven = 40
+// const preparationMinutesPerLayer = 2
+
+
+// function remainingMinutesInOven(actualMinutesInOven) {
+//   return expectedMinutesInOven - actualMinutesInOven
+// }
+
+
+// function preparationTimeInMinutes(numberOfLayers) {
+//    return numberOfLayers * preparationMinutesPerLayer
+//  }
+
+// function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
+//   return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven
+// }
+
+// console.log(totalTimeInMinutes(5, 5))
